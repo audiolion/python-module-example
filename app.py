bf69b9f35be34384a65869b624bb5f8bb11c3515
Ryan Castner <castner.rr@gmail.com>
2017-06-12 13:16:41 -0400

    example

---
+++ app.py
@@ -0,0 +1,6 @@
+from somepackage.utils import load_data
+
+
+def some_function(blah):
+    data = load_data(os.path(__file__))
+    # do more stuff
---
+++ somepackage/utils.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import pandas
+
+
+def load_data(file_path):
+    # do some file loading
+    return data
